f292fbe856fda8df60bc1264ccfd29839ef27dc9
jentzensky <jentzensky.pl@gmail.com>
2026-07-01 00:53:05 +0800

    Refresh PWA cache strategy

--- investment-advisor/sw.js
+++ investment-advisor/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sky-invest-v1";
+const CACHE_NAME = "sky-invest-v2-layout";
 const ASSETS = [
   "./",
   "./index.html",
@@ -10,6 +10,7 @@ const ASSETS = [
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
@@ -18,8 +19,17 @@ self.addEventListener("activate", (event) => {
       Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))),
     ),
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request)),
+  );
 });
